5ce6cc4d30c042fd3c2cead9229c4ddf73aabe4a
Jonathan Diamond <feros32@gmail.com>
2020-06-22 10:16:02 -0700

    Enable usermod_mpu6050

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -12,6 +12,7 @@
 //#include "usermod_v2_example.h"
 //#include "usermod_temperature.h"
 //#include "usermod_v2_empty.h"
+#include "usermod_mpu6050_imu.h"
 
 void registerUsermods()
 {
@@ -23,4 +24,5 @@ void registerUsermods()
   //usermods.add(new MyExampleUsermod());
   //usermods.add(new UsermodTemperature());
   //usermods.add(new UsermodRenameMe());
+  usermods.add(new MPU6050Driver());
 }
