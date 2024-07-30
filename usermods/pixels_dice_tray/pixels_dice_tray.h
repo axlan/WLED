7a40c02820ab133afd396d08f305e7ff01f18a42
jdiamond <feros32@gmail.com>
2024-07-30 01:09:30 +0000

    Initial menu working.

--- usermods/pixels_dice_tray/pixels_dice_tray.h
+++ usermods/pixels_dice_tray/pixels_dice_tray.h
@@ -51,10 +51,45 @@
   #define USERMOD_PIXELS_DICE_TRAY_TIMEOUT_MS 5*60*1000
 #endif
 
+#define WLED_DEBOUNCE_THRESHOLD      50 // only consider button input of at least 50ms as valid (debouncing)
+#define WLED_LONG_PRESS             600 // long press if button is released after held for at least 600ms
+#define WLED_DOUBLE_PRESS           350 // double press if another press within 350ms after a short press
+
 extern int getSignalQuality(int rssi);
 
 static constexpr size_t BLE_SCAN_DURATION_SEC = 4;
 static constexpr size_t BLE_TIME_BETWEEN_SCANS_SEC = 5;
+
+const uint8_t LIGHTNING_ICON_8X8[]  PROGMEM = {
+  0b00001111,
+  0b00010010,
+  0b00100100,
+  0b01001111,
+  0b10000001,
+  0b11110010,
+  0b00010100,
+  0b00011000,
+};
+
+const uint8_t BATTERY_ICON_8X8[]  PROGMEM = {
+  0b00000000,
+  0b00000000,
+  0b11111110,
+  0b10101011,
+  0b10101011,
+  0b11111110,
+  0b00000000,
+  0b00000000,
+};
+
+static constexpr size_t NUM_DICE = 2;
+// NOTE: The ordering is taken into account when referencing die 1 vs die 2.
+static std::array<std::string, NUM_DICE> configured_die_names = {
+  "Aurora",
+  "Midnight",
+};
+// The ordering here matches configured_die_names.
+static std::array<pixels::PixelsDieID, NUM_DICE> connected_die_ids = {0, 0};
 
 // The vectors to hold results queried from the library
 // Since vectors allocate data, it's more efficient to keep reusing objects
@@ -93,9 +128,12 @@ static uint16_t my_blink() {
 // speed/intesity, 2 colors, no palette, flags
 static const char _data_FX_MODE_MYBLINK[] PROGMEM = "MyBLINK@!,!;!,!;;01";
 
-
-
-// Need to check if this needs some synchronization.
+// These are updated in the main loop, but accessed by the effect functions as
+// well. My understand is that both of these accesses should be running on the
+// same "thread/task" since WLED doesn't directly create additional threads. The
+// exception would be network callbacks and interrupts, but I don't beleive
+// these accesses are triggered by those. If synchronization was needed, I could
+// look at the example in `requestJSONBufferLock()`.
 static pixels::RollUpdates dice_effect_state;
 
 extern uint16_t mode_breath();
@@ -105,7 +143,8 @@ static uint16_t basic_roll() {
   if (!dice_effect_state.empty()) {
     // Only keep last state.
     if (dice_effect_state.size() > 1) {
-      dice_effect_state.erase(dice_effect_state.begin(), dice_effect_state.begin() + dice_effect_state.size() - 1);
+      dice_effect_state[0] = dice_effect_state.back();
+      dice_effect_state.resize(1);
     }
 
     auto roll = dice_effect_state.end()->second;
@@ -154,21 +193,37 @@ class RollCountWidget
 {
   private:
     // Could make configurable if needed.
-    const int16_t xs = 0;
-    const int16_t ys = 0;
-    const uint16_t border_color = TFT_RED;
-    const uint16_t bar_color = TFT_GREEN;
-    const uint16_t bar_width = 6;
-    const uint16_t max_bar_height = 60;
+    int16_t xs = 0;
+    int16_t ys = 0;
+    uint16_t border_color = TFT_RED;
+    uint16_t bar_color = TFT_GREEN;
+    uint16_t bar_width = 6;
+    uint16_t max_bar_height = 60;
     unsigned roll_counts[20] = {0};
     unsigned total = 0;
     unsigned max_count = 0;
     
   public:
+   RollCountWidget(int16_t xs = 0, int16_t ys = 0,
+                   uint16_t border_color = TFT_RED,
+                   uint16_t bar_color = TFT_GREEN,
+                   uint16_t bar_width = 6,
+                   uint16_t max_bar_height = 60)
+       : xs(xs),
+         ys(ys),
+         border_color(border_color),
+         bar_color(bar_color),
+         bar_width(bar_width),
+         max_bar_height(max_bar_height) {}
 
-    void Clear() {
-      memset(roll_counts,0,sizeof(roll_counts));
-      total = 0;
+   void Clear() {
+     memset(roll_counts, 0, sizeof(roll_counts));
+     total = 0;
+     max_count = 0;
+    }
+
+    unsigned GetNumRolls() const {
+      return total;
     }
 
     void AddRoll(unsigned val){
@@ -201,6 +256,146 @@ class RollCountWidget
 };
 
 
+enum class ButtonType {
+  SINGLE,
+  DOUBLE,
+  LONG
+};
+
+class MenuBase {
+ public:
+  virtual void Update() = 0;
+
+  virtual void Draw(bool force_redraw) = 0;
+
+  virtual void HandleButton(ButtonType type, uint8_t b) = 0;
+};
+
+class DiceStatusMenu : public MenuBase {
+ public:
+  DiceStatusMenu()
+      : die_roll_counts{RollCountWidget{0, 20, TFT_BLUE, TFT_GREEN, 6, 40},
+                        RollCountWidget{0, SECTION_HEIGHT + 20, TFT_BLUE, TFT_GREEN, 6, 40}} {}
+
+  void Update() override {
+    for (size_t i = 0; i < NUM_DICE; i++) {
+      const auto die_id = connected_die_ids[i];
+      const auto connected = die_id != 0;
+
+      die_updated[i] = connected != die_connection_status[i];
+      die_connection_status[i] = connected;
+
+      if (connected) {
+        bool charging = false;
+        for (const auto& battery : battery_updates) {
+          if (battery.first == die_id) {
+            if (die_battery[i].battery_level == INVALID_BATTERY ||
+                battery.second.is_charging != die_battery[i].is_charging) {
+              die_updated[i] = true;
+            }
+            die_battery[i] = battery.second;
+          }
+        }
+
+        for (const auto& roll : roll_updates) {
+          if (roll.first == die_id &&
+              roll.second.state == pixels::RollState::ON_FACE) {
+            die_roll_counts[i].AddRoll(roll.second.current_face);
+            die_updated[i] = true;
+          }
+        }
+
+        for (const auto& battery : battery_updates) {
+          if (battery.first == die_id) {
+            die_battery[i] = battery.second;
+          }
+        }
+      }
+    }
+  }
+
+  void Draw(bool force_redraw) override {
+    // This could probably be optimized for partial redraws.
+    for (size_t i = 0; i < NUM_DICE; i++) {
+      const int16_t ys = SECTION_HEIGHT * i;
+      const auto die_id = connected_die_ids[i];
+      const auto connected = die_id != 0;
+      // Screen updates might be slow, yield in case network task needs to do
+      // work.
+      yield();
+      bool battery_update = connected && (millis() - last_update[i] > BATTERY_REFRESH_RATE_MS);
+      if (force_redraw || die_updated[i] || battery_update) {
+        last_update[i] = millis();
+        tft.fillRect(0, ys, TFT_WIDTH, SECTION_HEIGHT, TFT_BLACK);
+        tft.drawRect(0, ys, TFT_WIDTH, SECTION_HEIGHT, TFT_BLUE);
+        if (!connected) {
+          tft.setTextColor(TFT_RED);
+          tft.setCursor(2, ys + 4);
+          tft.setTextSize(2);
+          tft.println(configured_die_names[i].c_str());
+          tft.setCursor(2, tft.getCursorY());
+          tft.print("Waiting...");
+        } else {
+          tft.setTextColor(TFT_WHITE);
+          tft.setCursor(0, ys + 2);
+          tft.setTextSize(1);
+          tft.println(configured_die_names[i].c_str());
+          tft.print("Cnt ");
+          tft.print(die_roll_counts[i].GetNumRolls());
+          if (die_battery[i].battery_level != INVALID_BATTERY) {
+            tft.print(" Bat ");
+            tft.print(die_battery[i].battery_level);
+            tft.print("%");
+            if (die_battery[i].is_charging) {
+              tft.drawBitmap(tft.getCursorX(), tft.getCursorY(),
+                             LIGHTNING_ICON_8X8, 8, 8, TFT_YELLOW);
+            }
+          }
+          die_roll_counts[i].Draw();
+        }
+        die_updated[i] = false;
+      }
+    }
+  }
+
+  void HandleButton(ButtonType type, uint8_t b) override {};
+
+ private:
+  static constexpr long BATTERY_REFRESH_RATE_MS = 60 * 1000;
+  static constexpr int16_t SECTION_HEIGHT = TFT_HEIGHT / NUM_DICE;
+  static constexpr uint8_t INVALID_BATTERY = 0xFF;
+  std::array<long, NUM_DICE> last_update = {0};
+  std::array<bool, NUM_DICE> die_connection_status = {false};
+  std::array<bool, NUM_DICE> die_updated = {false};
+  std::array<pixels::BatteryEvent, NUM_DICE> die_battery = {
+      pixels::BatteryEvent{INVALID_BATTERY, false},
+      pixels::BatteryEvent{INVALID_BATTERY, false}};
+  std::array<RollCountWidget, NUM_DICE> die_roll_counts;
+};
+
+class MenuController {
+ public:
+  void HandleButton(ButtonType type, uint8_t b) {}
+
+  void Update() {
+    status_menu.Update();
+    status_menu.Draw(!menu_drawn);
+    menu_drawn = true;
+  }
+
+ private:
+  size_t current_index = 0;
+  bool menu_drawn = false;
+
+  DiceStatusMenu status_menu;
+
+};
+MenuController menu_ctrl;
+
+
+
+
+
 class PixelsDiceTrayUsermod : public Usermod {
  private:
   unsigned long lastTime = 0;
@@ -217,8 +412,6 @@ class PixelsDiceTrayUsermod : public Usermod {
   static constexpr size_t LINE_BUFFER_SIZE = TFT_CHAR_WIDTH + 1;  
 
   long lastUpdate = 0;
-
-  RollCountWidget roll_widget;
 
   // Set the pin to turn the backlight on or off if available.
   static void EnableBacklight(bool enable) {
@@ -342,20 +535,52 @@ class PixelsDiceTrayUsermod : public Usermod {
     pixels::GetDieRollUpdates(roll_updates);
     pixels::GetDieBatteryUpdates(battery_updates);
 
-    bool rolled = false;
-    for (const auto& roll: roll_updates) {
-      if (roll.second.state == pixels::RollState::ON_FACE) {
-        roll_widget.AddRoll(roll.second.current_face);
-        rolled = true;
+    // Go through list of connected die.
+    std::array<bool, NUM_DICE> die_connected = {false, false};
+    for (auto die_id : dice_list) {
+      // First check if we've already matched this ID to a connected die.
+      bool matched = false;
+      for (size_t i = 0; i < NUM_DICE; i++) {
+        if (die_id == connected_die_ids[i]) {
+          die_connected[i] = true;
+          matched = true;
+          break;
+        }
+      }
+
+      // If this isn't already matched, check if its name matches an expected name.
+      if (!matched) {
+        auto description = pixels::GetDieDescription(die_id);
+        for (size_t i = 0; i < NUM_DICE; i++) {
+          if (0 == connected_die_ids[i] && description.name == configured_die_names[i]) {
+            connected_die_ids[i] = die_id;
+            die_connected[i] = true;
+            break;
+          }
+        }
       }
     }
+
+    // Clear connected die that weren't still present.
+    bool all_found = true;
+    for (size_t i = 0; i < NUM_DICE; i++) {
+      if (!die_connected[i]) {
+        connected_die_ids[i] = 0;
+        all_found = false;
+      }
+    }
+
+    if (pixels::IsScanning() && all_found) {
+      pixels::StopScanning();
+    }
+    else if(!pixels::IsScanning() && !all_found) {
+      pixels::ScanForDice(BLE_SCAN_DURATION_SEC, BLE_TIME_BETWEEN_SCANS_SEC);
+    }
+
     // Add updates to the effect queue.
     dice_effect_state.insert(dice_effect_state.end(), roll_updates.begin(), roll_updates.end());
 
-    if (rolled) {
-      tft.fillScreen(TFT_BLACK);
-      roll_widget.Draw();
-    }
+    menu_ctrl.Update();
   }
 
   /*
@@ -470,6 +695,74 @@ class PixelsDiceTrayUsermod : public Usermod {
     // use "return !top["newestParameter"].isNull();" when updating Usermod with
     // new features
     return !top["TFT"].isNull();
+  }
+
+
+  /**
+   * handleButton() can be used to override default button behaviour. Returning true
+   * will prevent button working in a default way.
+   * Replicating button.cpp
+   */
+  bool handleButton(uint8_t b) override {
+    if (!enabled
+      || b > 1 // buttons 0,1 only
+      || buttonType[b] == BTN_TYPE_SWITCH
+      || buttonType[b] == BTN_TYPE_NONE
+      || buttonType[b] == BTN_TYPE_RESERVED
+      || buttonType[b] == BTN_TYPE_PIR_SENSOR
+      || buttonType[b] == BTN_TYPE_ANALOG
+      || buttonType[b] == BTN_TYPE_ANALOG_INVERTED) {
+      return false;
+    }
+
+    unsigned long now = millis();
+    static bool buttonPressedBefore[2] = { false };
+    static bool buttonLongPressed[2] = { false };
+    static unsigned long buttonPressedTime[2] = { 0 };
+    static unsigned long buttonWaitTime[2] = { 0 };
+
+    //momentary button logic
+    if (isButtonPressed(b)) { //pressed
+
+      if (!buttonPressedBefore) { buttonPressedTime[b] = now; }
+      buttonPressedBefore[b] = true;
+
+      if (now - buttonPressedTime[b] > WLED_LONG_PRESS) { //long press
+        menu_ctrl.HandleButton(ButtonType::LONG, b);
+        buttonLongPressed[b] = true;
+        return true;
+      }
+    }
+    else if (!isButtonPressed(b) && buttonPressedBefore) { //released
+
+      long dur = now - buttonPressedTime[b];
+      if (dur < WLED_DEBOUNCE_THRESHOLD) {
+        buttonPressedBefore[b] = false;
+        return true;
+      } //too short "press", debounce
+
+      bool doublePress = buttonWaitTime; //did we have short press before?
+      buttonWaitTime[b] = 0;
+
+      if (!buttonLongPressed) { //short press
+        // if this is second release within 350ms it is a double press (buttonWaitTime!=0)
+        if (doublePress) {
+          menu_ctrl.HandleButton(ButtonType::DOUBLE, b);
+        }
+        else {
+          buttonWaitTime[b] = now;
+        }
+      }
+      buttonPressedBefore[b] = false;
+      buttonLongPressed[b] = false;
+    }
+    // if 350ms elapsed since last press/release it is a short press
+    if (buttonWaitTime && now - buttonWaitTime[b] > WLED_DOUBLE_PRESS && !buttonPressedBefore) {
+      buttonWaitTime[b] = 0;
+      menu_ctrl.HandleButton(ButtonType::SINGLE, b);
+    }
+
+    return true;
   }
 
   /*
